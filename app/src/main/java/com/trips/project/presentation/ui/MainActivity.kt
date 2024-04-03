11db0ed45407aa2f44761f1fc15f2733fc643eb9
shilat183 <shilats2001@gmail.com>
2024-04-03 21:01:37 +0300

    added Navgraph, ProfileEdit, MainActivity

---
+++ app/src/main/java/com/trips/project/presentation/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.trips.project.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.navigation.Navigation.findNavController
+import com.google.android.libraries.places.api.Places
+import com.trips.project.R
+import me.ibrahimsn.lib.SmoothBottomBar
+
+class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+                Places.initialize(applicationContext, "AIzaSyCT1LawqV43MkqtMqwUuKVSArVpRNdQ5aA")
+                val bottomNavigationView1: BottomNavigationView
+                val menu: Menu
+                bottomNavigationView1 = findViewById(R.id.bottom_navigation1)
+                menu = bottomNavigationView1.menu
+                val bottomNavigationView = findViewById<SmoothBottomBar>(R.id.bottom_navigation)
+                bottomNavigationView.visibility = View.VISIBLE
+                val navController = findNavController(this, R.id.nav_host_fragment)
+                bottomNavigationView.setupWithNavController(menu , navController)
+        }
+
+}
---
+++ app/src/main/java/com/trips/project/presentation/ui/fragments/ProfileEditFragment.kt
@@ -0,0 +1,128 @@
+package com.trips.project.presentation.ui.fragments
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.trips.project.R
+import com.trips.project.presentation.ui.LoginActivity
+import com.squareup.picasso.Picasso
+
+class ProfileEditFragment : Fragment() {
+
+    private lateinit var imgProfile: ImageView
+    private var filePath: Uri? = null
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageReference: StorageReference
+    private lateinit var edtName: EditText
+    private var selectedImageUri: Uri? = null
+    companion object {
+        private const val REQUEST_CODE_GALLERY = 1
+    }
+    private val imagePickerLauncher = registerForActivityResult<String, Uri>(
+        ActivityResultContracts.GetContent(),
+        object : ActivityResultCallback<Uri?> {
+            override fun onActivityResult(result: Uri?) {
+                if (result != null) {
+                    try {
+                        result?.let {
+                            Picasso.get().load(it).into(imgProfile)
+                            selectedImageUri = it
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    )
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile_edit, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage.reference
+
+        imgProfile = view.findViewById(R.id.profile_image)
+        val btnUpdate = view.findViewById<Button>(R.id.edit_profile_name_button)
+        val btnLogout = view.findViewById<Button>(R.id.logout_button)
+        edtName = view.findViewById(R.id.profile_name_edittext)
+        // Initialize with current user info
+        val user = mAuth.currentUser
+        user?.let {
+            if (user.photoUrl != null) {
+                Picasso.get().load(user.photoUrl).into(imgProfile)
+            }
+            edtName.setText(user.displayName)
+        }
+
+        imgProfile.setOnClickListener { chooseImage() }
+
+        btnUpdate.setOnClickListener { updateProfile() }
+
+        btnLogout.setOnClickListener { logout() }
+
+        return view
+    }
+
+    private fun chooseImage() {
+        imagePickerLauncher.launch("image/*")
+    }
+
+
+    private fun updateProfile() {
+        val name = edtName.text.toString().trim()
+        filePath?.let { fileUri ->
+            val ref = storageReference.child("profileImages/${mAuth.currentUser?.uid}")
+            ref.putFile(fileUri).addOnSuccessListener { taskSnapshot ->
+                ref.downloadUrl.addOnSuccessListener { uri ->
+                    val profileUpdate = UserProfileChangeRequest.Builder()
+                            .setDisplayName(name)
+                            .setPhotoUri(uri)
+                            .build()
+                    mAuth.currentUser?.updateProfile(profileUpdate)?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "Profile Updated", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    Picasso.get().load(uri).into(imgProfile)
+                }
+            }.addOnFailureListener {
+                Toast.makeText(context, "Failed to upload image", Toast.LENGTH_SHORT).show()
+            }
+        } ?: run {
+            // Update name only
+            val profileUpdate = UserProfileChangeRequest.Builder()
+                    .setDisplayName(name)
+                    .build()
+            mAuth.currentUser?.updateProfile(profileUpdate)?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Profile Updated", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun logout() {
+        mAuth.signOut()
+        startActivity(Intent(context, LoginActivity::class.java))
+        activity?.finish()
+    }
+}
